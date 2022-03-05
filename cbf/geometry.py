ac34841f9e0da3004a53f98c63906e61f420429a
Neelaksh-007 <lzrs009gamer@gmail.com>
2022-03-05 16:52:27 +0530

    feature: Added the Transform and Bounding Box objects
    TODO:
    1. Create the velocity controlled CBF class
    2. Integrate the CBF with a synchronous CARLA simulation based on set_target_velocity()
    
    bugs: <same as last commit>
    
    fix: no fixes made in this commit.

--- cbf/geometry.py
+++ cbf/geometry.py
@@ -10,6 +10,7 @@ author: Neelaksh Singh
 
 from turtle import forward
 from euclid import *
+from sympy import Point3D
 
 class Rotation():
     """
@@ -26,6 +27,9 @@ class Rotation():
         self.pitch = pitch
         self.yaw = yaw
         self.roll = roll
+        self.heading = self.yaw
+        self.attitude = self.pitch
+        self.bank = self.roll
         # Euclid uses the h, a, b system which corresponds to
         # euler angles as follows:
         # Heading  => Yaw
@@ -45,10 +49,10 @@ class Rotation():
         return not self.__eq__(other)
     
     def __str__(self):
-        return f"{type(self).__name__}(roll = {self.roll}, pitch = {self.pitch}, yaw = {self.yaw}, quaternion = {self.quaternion})"
+        return f"{type(self).__name__}(roll = {self.roll}, pitch = {self.pitch}, yaw = {self.yaw}, quaternion = {self._quaternion})"
     
     def __repr__(self):
-        return self.__str__ + "\n"
+        return f"{type(self).__name__}(roll = {self.roll}, pitch = {self.pitch}, yaw = {self.yaw}, quaternion = {self._quaternion})\n"
 
     def get_quaternion(self):
         return self._quaternion
@@ -64,3 +68,45 @@ class Rotation():
     def get_forward_vector(self):
         forward = Vector3(1.0, 0, 0)
         return self._quaternion * forward
+
+class Transform():
+    """
+    A CARLA style implementation of the Transform object by wrapping
+    over the euclid Matrix4 class. 
+    """
+    def __init__(self, location=Vector3(), rotation=Rotation()):
+        self.location = location
+        self.rotation = rotation
+        self._matrix = Matrix4()
+        self._matrix.rotate_euler(rotation.heading, rotation.attitude, rotation.bank)
+        self._matrix.translate(location.x, location.y, location.z)
+    
+    def __eq__(self, other):
+        return self.location == other.location and self.rotation == other.rotation
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+    def __str__(self, other):
+        return self._matrix.__str__
+
+    def __repr__(self, other):
+        return self._matrix.__repr__
+    
+    def transform(self, p = Point3()):
+        return self._matrix.transform(p)
+    
+    def get_forward_vector(self):
+        return self.rotation.get_forward_vector()
+
+    def get_inverse_matrix(self):
+        return self._matrix.inverse()
+
+    def get_matrix(self):
+        return self._matrix
+
+    def get_right_vector(self):
+        return self.rotation.get_right_vector()
+
+    def get_up_vector(self):
+        return self.rotation.get_up_vector()
