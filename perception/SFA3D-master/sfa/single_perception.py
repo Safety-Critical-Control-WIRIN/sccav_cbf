9a1fcd8fd8cc3bf532899faba4fde52a79d82223
Shyam-pi <pi.shyamsundar@gmail.com>
2022-05-09 18:25:49 +0530

    Adding the SFA3D perception package
    Added the 3D perception stack folder, without the KITTI dataset

---
+++ perception/SFA3D-master/sfa/single_perception.py
@@ -0,0 +1,167 @@
+import argparse
+import sys
+import os
+import time
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+from easydict import EasyDict as edict
+import cv2
+import torch
+import numpy as np
+
+src_dir = os.path.dirname(os.path.realpath(__file__))
+while not src_dir.endswith("sfa"):
+    src_dir = os.path.dirname(src_dir)
+if src_dir not in sys.path:
+    sys.path.append(src_dir)
+
+from data_process.kitti_dataloader import create_test_dataloader
+from models.model_utils import create_model
+from utils.misc import make_folder, time_synchronized
+from utils.evaluation_utils import decode, post_processing, draw_predictions, convert_det_to_real_values
+from utils.torch_utils import _sigmoid
+import config.kitti_config as cnf
+from data_process.transformation import lidar_to_camera_box
+from utils.visualization_utils import merge_rgb_to_bev, show_rgb_image_with_boxes
+from data_process.kitti_data_utils import Calibration
+
+
+def parse_test_configs():
+    parser = argparse.ArgumentParser(description='Testing config for the Implementation')
+    parser.add_argument('--saved_fn', type=str, default='fpn_resnet_18', metavar='FN',
+                        help='The name using for saving logs, models,...')
+    parser.add_argument('-a', '--arch', type=str, default='fpn_resnet_18', metavar='ARCH',
+                        help='The name of the model architecture')
+    parser.add_argument('--pretrained_path', type=str,
+                        default='C:/Shyam/SFA3D-master/SFA3D-master/checkpoints/fpn_resnet_18/fpn_resnet_18_epoch_300.pth', metavar='PATH',
+                        help='the path of the pretrained checkpoint')
+    parser.add_argument('--K', type=int, default=50,
+                        help='the number of top K')
+    parser.add_argument('--no_cuda', action='store_true',
+                        help='If true, cuda is not used.')
+    parser.add_argument('--gpu_idx', default=0, type=int,
+                        help='GPU index to use.')
+    parser.add_argument('--num_samples', type=int, default=None,
+                        help='Take a subset of the dataset to run and debug')
+    parser.add_argument('--num_workers', type=int, default=1,
+                        help='Number of threads for loading data')
+    parser.add_argument('--batch_size', type=int, default=1,
+                        help='mini-batch size (default: 4)')
+    parser.add_argument('--peak_thresh', type=float, default=0.2)
+    parser.add_argument('--save_test_output', action='store_true',
+                        help='If true, the output image of the testing phase will be saved')
+    parser.add_argument('--output_format', type=str, default='image', metavar='PATH',
+                        help='the type of the test output (support image or video)')
+    parser.add_argument('--output_video_fn', type=str, default='out_fpn_resnet_18', metavar='PATH',
+                        help='the video filename if the output format is video')
+    parser.add_argument('--output-width', type=int, default=608,
+                        help='the width of showing output, the height maybe vary')
+
+    configs = edict(vars(parser.parse_args()))
+    configs.pin_memory = True
+    configs.distributed = False  # For testing on 1 GPU only
+
+    configs.input_size = (608, 608)
+    configs.hm_size = (152, 152)
+    configs.down_ratio = 4
+    configs.max_objects = 50
+
+    configs.imagenet_pretrained = False
+    configs.head_conv = 64
+    configs.num_classes = 3
+    configs.num_center_offset = 2
+    configs.num_z = 1
+    configs.num_dim = 3
+    configs.num_direction = 2  # sin, cos
+
+    configs.heads = {
+        'hm_cen': configs.num_classes,
+        'cen_offset': configs.num_center_offset,
+        'direction': configs.num_direction,
+        'z_coor': configs.num_z,
+        'dim': configs.num_dim
+    }
+    configs.num_input_features = 4
+
+    ####################################################################
+    ##############Dataset, Checkpoints, and results dir configs#########
+    ####################################################################
+    configs.root_dir = '../'
+    configs.dataset_dir = os.path.join(configs.root_dir, 'dataset', 'kitti')
+
+    if configs.save_test_output:
+        configs.results_dir = os.path.join(configs.root_dir, 'results', configs.saved_fn)
+        make_folder(configs.results_dir)
+
+    return configs
+
+
+class Detection:
+    model = None
+    configs = None
+
+    def __init__(self):
+        self.configs = parse_test_configs()
+        self.model = create_model(self.configs)
+        print('\n\n' + '-*=' * 30 + '\n\n')
+        assert os.path.isfile(self.configs.pretrained_path), "No file at {}".format(self.configs.pretrained_path)
+        self.model.load_state_dict(torch.load(self.configs.pretrained_path, map_location='cpu'))
+        print('Loaded weights from {}\n'.format(self.configs.pretrained_path))
+
+        self.configs.device = torch.device('cpu' if self.configs.no_cuda else 'cuda:{}'.format(self.configs.gpu_idx))
+        self.model = self.model.to(device=self.configs.device)
+        self.model.eval()
+
+    def get_detections(self, lidar):
+        with torch.no_grad():
+            # metadatas, bev_maps, img_rgbs = batch_data
+            bev_map = lidar
+            input_bev_map = bev_map.to(self.configs.device, non_blocking=True).float()
+            t1 = time_synchronized()
+            outputs = self.model(input_bev_map)
+            outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
+            outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
+            # detections size (batch_size, K, 10)
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
+                                outputs['dim'], K=self.configs.K)
+            detections = detections.cpu().numpy().astype(np.float32)
+            detections = post_processing(detections, self.configs.num_classes, self.configs.down_ratio, self.configs.peak_thresh)
+            t2 = time_synchronized()
+
+            print('\tDone inferencing, time: {:.1f}ms, speed {:.2f}FPS'.format((t2 - t1) * 1000, 1 / (t2 - t1)))
+
+            detections = detections[0]  # only first batch
+
+            return detections
+
+    def generate_obstacle_map(self, lidar):
+            detections = self.get_detections(lidar)
+            print("Number of obstacles detected = " + str(len(detections[0]) + len(detections[1]) + len(detections[2])))
+            bev_map = lidar
+            # img_rgb = img
+            # Draw prediction in the image
+            bev_map = (bev_map.squeeze().permute(1, 2, 0).numpy() * 255).astype(np.uint8)
+            bev_map = cv2.resize(bev_map, (cnf.BEV_WIDTH, cnf.BEV_HEIGHT))
+            bev_map = draw_predictions(bev_map, detections.copy(), self.configs.num_classes)
+
+            # Rotate the bev_map
+            bev_map = cv2.rotate(bev_map, cv2.ROTATE_180)
+
+            """Bounding box depiction on camera images to be added"""
+            # img_rgb = cv2.resize(img_rgb, (img_rgb.shape[1], img_rgb.shape[0]))
+            # img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
+            # calib = 1
+            # kitti_dets = convert_det_to_real_values(detections)
+            # if len(kitti_dets) > 0:
+            #     kitti_dets[:, 1:] = lidar_to_camera_box(kitti_dets[:, 1:], calib.V2C, calib.R0, calib.P2)
+            #     img_bgr = show_rgb_image_with_boxes(img_bgr, kitti_dets, calib)
+            #
+            # out_img = merge_rgb_to_bev(img_bgr, bev_map, output_width=self.configs.output_width)
+
+            return bev_map
+            # print('\n[INFO] Press n to see the next sample >>> Press Esc to quit...\n')
+
+    if __name__ == '__main__':
+        print('finish')
